75713f98be395c2b52230c3dec0e429bb926d0ce
kacha930 <robiwere20@gmail.com>
2024-07-22 04:45:54 +0300

    succes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ("Guadalupe", "Ollie", "Aki")
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+     console.log(i);}}
+  
+  module.exports = {
+    writeCards,
+    countDown
+};
